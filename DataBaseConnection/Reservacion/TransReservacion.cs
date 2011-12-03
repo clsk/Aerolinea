adc0175cfcd627a467cee3f9bfaf912547cface0
Alan Alvarez <aalvarez@aliensoft.net>
2011-12-03 16:40:27 -0400

    Agregar TransReservacion

---
+++ DataBaseConnection/Reservacion/TransReservacion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataBaseConnection
+{
+    class TransReservacion
+    {
+        public string NombrePersona
+        {
+            get;
+            set;
+        }
+
+        public string ApellidosPersona
+        {
+            get;
+            set;
+        }
+
+        public string PasaportePersona
+        {
+            get;
+            set;
+        }
+
+        // TransVuelo vuelo;
+        TransAsiento asiento;
+        Persona persona;
+        TransUsuario usuario_registrador;
+    }
+}
