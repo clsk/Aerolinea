8b5a28dd5e300def62d2c3d747da69f206cc8951
Jhuliano Moreno <jhuliano.moreno@gmail.com>
2011-12-03 17:04:18 -0400

    Interface Reservación

---
+++ DataBaseConnection/Reservacion/IReservacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataBaseConnection
+{
+    interface IReservacion
+    {
+        int IdVuelo {get; set; }
+
+        int idAsiento { get; set; }
+        //bahusahduashdasi;dasd
+
+    }
+}
