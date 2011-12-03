7393ecc3e0fd2991a1464491e8b9d2f31341b1a1
Alan Alvarez <aalvarez@aliensoft.net>
2011-12-03 18:24:10 -0400

    Add TransAvion

---
+++ DataBaseConnection/Avion/TransAvion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataBaseConnection
+{
+    public class TransAvion: IAvion
+    {
+
+        public TransAvion(int idAvion)
+        {
+        }
+
+        private Avion avion;
+
+        private MarcaAvion marca;
+
+        public MarcaAvion Marca { get; set;  }
+
+        private SerieAvion serie;
+
+        public SerieAvion Serie { get; set;  }
+
+        private List<PlantaAvion> plantas;
+
+        public List<PlantaAvion> Plantas { get; set;  }
+
+        public string NombreMarca { get; set;}
+
+        public string NombreSerie { get; set; }
+
+        public int ID { get; set; }
+
+    }
+}
