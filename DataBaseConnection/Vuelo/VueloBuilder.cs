1e439542f8261e80d37d03e5d316cd1354051f03
Alan Alvarez <aalvarez@aliensoft.net>
2011-12-03 20:24:04 -0400

    Create AbstractBuilder and VueloBuilder

---
+++ DataBaseConnection/Vuelo/VueloBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataBaseConnection
+{
+    /*
+    class VueloBuilder : AbstractBuilder<TransVuelo, Vuelo, DALVuelo>
+    {
+    }
+     * */
+}
