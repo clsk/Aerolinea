9d98d68c3c55245679894c515cca403a43bff822
Alan Alvarez <aalvarez@aliensoft.net>
2011-12-05 16:49:09 -0400

    AsientoFactory

---
+++ DataLayer/Asiento/AsientoFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    public class AsientoFactory : AbstractFactory<IAsiento, TransAsiento, Asiento>
+    {
+        AsientoFactory()
+            : base(DALAsiento.GetAsientoFromID)
+        {
+        }
+    }
+}
