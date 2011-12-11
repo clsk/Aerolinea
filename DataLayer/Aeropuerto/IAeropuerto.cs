e5015553a01fa28163a8588b39287a08c65b451c
Jhuliano Moreno <jhuliano.moreno@gmail.com>
2011-12-11 14:46:31 -0400

    Aeropuerto

---
+++ DataLayer/Aeropuerto/IAeropuerto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    public interface IAeropuerto
+    {
+        string NombreAero { get; set; }
+        string Siglas { get; set; }
+        Ciudad ciudad { get; set; }
+        void flush();
+    }
+}
---
+++ DataLayer/Aeropuerto/TransAeropuerto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    class TransAeropuerto : IAeropuerto
+    {
+        private Aeropuerto persAeropuerto;
+        public TransAeropuerto(Aeropuerto aeropuerto)
+        {
+            persAeropuerto = aeropuerto;
+        }
+
+
+        #region IAeropuerto Members
+
+        public string NombreAero
+        {
+            get
+            {
+                return persAeropuerto.NombreAeropuerto;
+            }
+            set
+            {
+                persAeropuerto.NombreAeropuerto = value;
+            }
+        }
+
+        public string Siglas
+        {
+            get
+            {
+                return persAeropuerto.Siglas;
+            }
+            set
+            {
+                persAeropuerto.Siglas = value;
+            }
+        }
+
+        public Ciudad ciudad
+        {
+            get
+            {
+                return persAeropuerto.Ciudad;
+            }
+            set
+            {
+                persAeropuerto.Ciudad = value;
+            }
+        }
+
+        public void flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
