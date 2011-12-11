e90a99d74b0b162d978a29b848ce442b97fbc5ed
Alan Alvarez <aalvarez@aliensoft.net>
2011-12-11 18:38:03 -0400

    Base class and Interface for Transient classes

---
+++ DataLayer/AbstractTrans.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+
+    public class AbstractTrans<PersistentType>
+    {
+        internal AbstractTrans(PersistentType persistent_object)
+        {
+            persistent = persistent_object;
+        }
+        protected PersistentType persistent;
+
+        public PersistentType PersistentObject
+        {
+            get { return persistent; }
+        }
+
+
+        public void Flush()
+        {
+            // TODO:
+            // receive Func<void, PersistentType> as parameter, then func(persistent_object)
+            throw new NotImplementedException();
+        }
+    }
+}
