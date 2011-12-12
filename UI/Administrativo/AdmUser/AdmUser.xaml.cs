51512ba134463e99ae80dde824b5bd600847f84e
Jhuliano Moreno <jhuliano.moreno@gmail.com>
2011-12-12 19:39:29 -0400

    Pantallas de AdmUser

---
+++ UI/Administrativo/AdmUser/AdmUser.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI.Administrativo.AdmUser
+{
+    /// <summary>
+    /// Interaction logic for AdmUser.xaml
+    /// </summary>
+    public partial class AdmUser : Window
+    {
+        public AdmUser()
+        {
+            InitializeComponent();
+        }
+
+        private void btmAtras_Click(object sender, RoutedEventArgs e)
+        {
+            MainAdm prevWin = new MainAdm();
+            prevWin.Top = this.Top;
+            prevWin.Left = this.Top;
+            prevWin.Show();
+            this.Close();
+        }
+
+        private void btmNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            NewUser prevWin = new NewUser();
+            prevWin.Top = this.Top;
+            prevWin.Left = this.Left;
+            prevWin.Show();
+            this.Close();
+        }
+    }
+}
---
+++ UI/Administrativo/AdmUser/NewUser.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI.Administrativo.AdmUser
+{
+    /// <summary>
+    /// Interaction logic for NewUser.xaml
+    /// </summary>
+    public partial class NewUser : Window
+    {
+        public NewUser()
+        {
+            InitializeComponent();
+        }
+
+        private void tbNombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btmNewUser.IsEnabled = VerifDatos();
+        }
+
+        private void tbLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btmNewUser.IsEnabled = VerifDatos();
+        }
+
+        private void tbPass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            btmNewUser.IsEnabled = VerifDatos();
+        }
+
+        private void btmNewUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private bool VerifDatos()
+        {
+            if (tbNombre.Text != "")
+                if (tbLogin.Text != "")
+                    if (tbPass.Password.ToString().Length >= 6)
+                        return true;
+            return false;
+        }
+
+        private void btmAtras_Click(object sender, RoutedEventArgs e)
+        {
+            AdmUser prevWin = new AdmUser();
+            prevWin.Top = this.Top;
+            prevWin.Left = this.Left;
+            prevWin.Show();
+            this.Close();
+        }
+    }
+}
