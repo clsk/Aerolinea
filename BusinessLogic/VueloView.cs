acd1fceb16e137620153b0340829c6994622ec91
Jhuliano Moreno <jhuliano.moreno@gmail.com>
2011-12-12 23:02:55 -0400

    views

---
+++ BusinessLogic/VueloView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataLayer;
+
+namespace BusinessLogic
+{
+    class VueloView : TransVuelo
+    {
+        public VueloView(Vuelo persistent_object) : base(persistent_object)
+        {
+        }
+
+        public string DisplayPuertos
+        {
+            get { return  ; }
+        }
+    }
+}
