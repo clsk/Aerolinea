7c47fd85a337f44d630a2c47ad9d13ba5c747e28
Alan Alvarez <aalvarez@aliensoft.net>
2011-12-15 16:16:45 -0400

    Move to UI

---
+++ UI/AeropuertoView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataLayer;
+
+namespace UI
+{
+    public class AeropuertoView : TransAeropuerto
+    {
+        public AeropuertoView(Aeropuerto persistent_object) : base(persistent_object)
+        {
+        }
+
+        public string DisplayName
+        {
+            get { return Nombre + " (" + Siglas + "), " + Ciudad.NombreCiudad + ", " + Ciudad.Pai.NombrePais; }
+        }
+    }
+}
